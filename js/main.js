f1a35fab0432d1b2c9d27914a9a4a89d959a9e8b
shannon8611 <shannon8611@yahoo.com>
2017-11-25 22:34:43 -0600

    Add splitchar jquery update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('#fullpage').fullpage();
 
     // This is the Splitchar jQuery Plugin that splits the characters of an element and allows you to style them individually.
-
+    $('.splitchar').splitchar();
     // This is my custom jQuery code that doubles the social media icons when hovered upon and returns it back to it's initial size when no longer hovered upon.
     $('.social').hover(function () {
         $(this).css('transform', 'scale(1.5,1.5)');
