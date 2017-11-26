56706508c6e0b67b7dd2738b830b0eb0f6fbf921
shannon8611 <shannon8611@yahoo.com>
2017-11-25 20:05:05 -0600

    add jquery plugins and custom jquery

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // This is the Fullpage jQuery plugin that provides an smooth transtion between sections on your page.
+    $('#fullpage').fullpage();
+
+    // This is the Splitchar jQuery Plugin that splits the characters of an element and allows you to style them individually.
+
+    // This is my custom jQuery code that doubles the social media icons when hovered upon and returns it back to it's initial size when no longer hovered upon.
+    $('.social').hover(function () {
+        $(this).css('transform', 'scale(1.5,1.5)');
+    }, function () {
+        $(this).css('transform', 'scale(1,1)');
+    });
+});
